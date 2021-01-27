327ab26b4d8ffe6a9ffdb65e042c8c3906eabb48
Victor Belikov <v1ctor.belikov@gmail.com>
2021-01-27 17:32:27 +0300

    Section 25: Auth functionality and so on

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -22,7 +22,9 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      headers: { Authorization: `Bearer ${this.props.token}` }
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -50,7 +52,9 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts')
+    fetch(`http://localhost:8080/feed/posts?page=${page}`, {
+      headers: { Authorization: `Bearer ${this.props.token}` }
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -59,7 +63,7 @@ class Feed extends Component {
       })
       .then((resData) => {
         this.setState({
-          posts: resData.posts,
+          posts: resData.posts.map((p) => ({ ...p, imageUrl: p.imageUrl })),
           totalPosts: resData.totalItems,
           postsLoading: false,
         });
@@ -69,7 +73,11 @@ class Feed extends Component {
 
   statusUpdateHandler = (event) => {
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      method: 'PATCH',
+      headers: { Authorization: `Bearer ${this.props.token}`, 'Content-Type': 'application/json' },
+      body: JSON.stringify({ status: this.state.status }),
+    })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
@@ -109,7 +117,8 @@ class Feed extends Component {
     let url = 'http://localhost:8080/feed/post';
     let method = 'POST';
     if (this.state.editPost) {
-      url = 'URL';
+      url = `http://localhost:8080/feed/post/${this.state.editPost._id}`;
+      method = 'PUT';
     }
 
     const formData = new FormData();
@@ -119,6 +128,7 @@ class Feed extends Component {
 
     fetch(url, {
       method,
+      headers: { Authorization: `Bearer ${this.props.token}` },
       // When we send JSON
       // headers: { 'Content-Type': 'application/json' },
       // body: JSON.stringify({ title: postData.title, content: postData.content }),
@@ -174,7 +184,10 @@ class Feed extends Component {
 
   deletePostHandler = (postId) => {
     this.setState({ postsLoading: true });
-    fetch('URL')
+    fetch(`http://localhost:8080/feed/post/${postId}`, {
+      method: 'DELETE',
+      headers: { Authorization: `Bearer ${this.props.token}` },
+    })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
